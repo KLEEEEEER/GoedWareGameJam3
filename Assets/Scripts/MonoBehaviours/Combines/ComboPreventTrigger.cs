2de13c92b6385ff61be00fc6023b87a0993ef2e4
KLEEEEEER <gonzalx_747@mail.ru>
2022-01-11 21:34:02 +0900

    Added shader effects and starting trigger behaviour

--- Assets/Scripts/MonoBehaviours/Combines/ComboPreventTrigger.cs
+++ Assets/Scripts/MonoBehaviours/Combines/ComboPreventTrigger.cs
@@ -26,6 +26,7 @@ namespace GoedWareGameJam3.MonoBehaviours.Combines
                     _changeIndex = Array.IndexOf(_rulesSettings.Rules, comboPreventingRule);
                 }
             }
+            _renderer.material = _rulesSettings.Rules[_changeIndex].Material;
         }
 
         private void OnTriggerEnter(Collider other)
